81f3cff0622b903a0411c3e3b8261f73c582a345
Claire-Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-25 18:42:40 -0400

    added dates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// Copyright (c) 202X Claire Bedrossian All rights reserved
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: XXX 202X
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Unit2-04-PWA/sw.js", {
